d52621acfc92dbf64a96b7807f10058d7a10cc7e
jaycikey <138725941+jaycikey@users.noreply.github.com>
2023-11-04 18:32:08 -0400

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
+form.addEventListner("submit", event => {
+event.preventDefault();
 
+const userData = {};
+
+form.elements.forEach(element => {
+    if(element.type !== "submit"){
+        userData[element.name] = element.value;
+        };
+    if(!userData.email || !userData.password){
+        alert("всі поля повинні бути заповнені.");
+        return;
+        };
+});
+
+console.log(userData);
+form.reset();
+});

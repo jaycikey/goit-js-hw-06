041912d616e3d4b2ae45d257c08d3b9ff8e0c2a5
jaycikey <138725941+jaycikey@users.noreply.github.com>
2023-11-04 12:21:17 -0400

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById("ingredients");
+
+const liElements = ingredients.map(ingredient => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  return li;
+});
+ingredientsList.append(...liElements);

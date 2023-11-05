23d58b152231103c79b5890e18237237c1c90a93
jaycikey <138725941+jaycikey@users.noreply.github.com>
2023-11-05 12:44:32 -0500

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputRef = document.querySelector('#controls input');
+const createButtonRef = document.querySelector('button[data-create]');
+const destroyButtonRef = document.querySelector('button[data-destroy]');
+const boxesContainerRef = document.querySelector('#boxes');
+
+createButtonRef.addEventListener('click', createBoxes);
+destroyButtonRef.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = inputRef.value;
+  const boxes = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement('div');
+    const size = 30 + i * 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+
+  boxesContainerRef.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesContainerRef.innerHTML = '';
+}
